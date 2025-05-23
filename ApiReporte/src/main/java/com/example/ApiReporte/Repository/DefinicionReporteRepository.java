d835e385d77041c83dd8b14bee244eb288fdd765
Gabriel Duran <gabrieldurans999@gmail.com>
2025-05-23 17:22:34 -0400

    Creacion de repository

---
+++ ApiReporte/src/main/java/com/example/ApiReporte/Repository/DefinicionReporteRepository.java
@@ -0,0 +1,9 @@
+package com.example.ApiReporte.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ApiReporte.Model.DefinicionReporte;
+
+public interface DefinicionReporteRepository extends JpaRepository <DefinicionReporte, Integer> {
+
+}
---
+++ ApiReporte/src/main/java/com/example/ApiReporte/Repository/HistoricoReporteRepository.java
@@ -0,0 +1,9 @@
+package com.example.ApiReporte.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.ApiReporte.Model.HistoricoReporte;
+
+public interface HistoricoReporteRepository extends JpaRepository<HistoricoReporte, Integer>{
+
+}
